f0ef0dcf2c620a8ee98b9c7d07aabfe21035ce5f
TechAoba <TechAoba@163.com>
2026-03-05 13:56:07 +0800

    对敌人造成伤害

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int currentHealth;
+    public int maxHealth;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void ChangeHealth(int amount)
+    {
+        currentHealth += amount;
+        if (currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+        if (currentHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
