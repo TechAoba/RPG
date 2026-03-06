9234984998794709ff6bc3a79f83446d32258bc8
TechAoba <TechAoba@163.com>
2026-03-06 15:00:14 +0800

    技能树 技能逻辑器编写

---
+++ Assets/Scripts/SkillTree/SkillTreeManager.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class SkillTreeManager : MonoBehaviour
+{
+    public SkillSlot[] skillSlots;
+    public TMP_Text pointsText;
+    public int availablePoints;
+
+    private void OnEnable()
+    {
+        SkillSlot.OnAbilityPointSpent += HandleAbilityPointsSpent;
+        SkillSlot.OnSkillMaxed += HandleSkillMaxed;
+    }
+    private void OnDisable()
+    {
+        SkillSlot.OnAbilityPointSpent -= HandleAbilityPointsSpent;
+        SkillSlot.OnSkillMaxed -= HandleSkillMaxed;
+    }
+
+
+    private void Start()
+    {
+        foreach (SkillSlot slot in skillSlots)
+        {
+            slot.skillButton.onClick.AddListener(() => 
+            {
+                if (availablePoints > 0)
+                {
+                    slot.TryUpgradeSkill();
+                }
+            });
+        }
+        UpdateAbilityPoints(0);
+    }
+    
+    private void HandleAbilityPointsSpent(SkillSlot skillSlot)
+    {
+        if (availablePoints > 0)
+        {
+            UpdateAbilityPoints(-1);
+        }
+    }
+
+    private void HandleSkillMaxed(SkillSlot skillSlot)
+    {
+        foreach (SkillSlot slot in skillSlots)
+        {
+            if (!slot.isUnlocked && slot.CanUnlockSkill())
+            {
+                slot.Unlock();
+            }
+        }
+    }
+
+    public void UpdateAbilityPoints(int amount)
+    {
+        availablePoints += amount;
+        pointsText.text = "Points: " + availablePoints;
+    }
+}
