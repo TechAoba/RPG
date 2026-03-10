970308636c8a637d77f3bd9bab231d4a862bf4df
TechAoba <TechAoba@163.com>
2026-03-10 11:43:32 +0800

    填充库存物品

--- Assets/Scripts/Inventory&Shop/Loot.cs
+++ Assets/Scripts/Inventory&Shop/Loot.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,6 +10,7 @@ public class Loot : MonoBehaviour
     public Animator anim;
 
     public int quantity;
+    public static event Action<ItemSO, int> OnItemLooted;
 
     private void OnValidate()
     {
@@ -23,6 +25,7 @@ public class Loot : MonoBehaviour
         if (collision.CompareTag("Player"))
         {
             anim.Play("LootPickup");
+            OnItemLooted?.Invoke(itemSO, quantity);
             Destroy(gameObject, .5f);
         }
     }
