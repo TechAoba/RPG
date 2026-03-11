05bfe25c19057224feb2b4d3b0a61db2c9979bf6
TechAoba <TechAoba@163.com>
2026-03-11 15:40:29 +0800

    物品信息面板

--- Assets/Scripts/Inventory&Shop/Shop/ShopSlot.cs
+++ Assets/Scripts/Inventory&Shop/Shop/ShopSlot.cs
@@ -2,15 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class ShopSlot : MonoBehaviour
+public class ShopSlot : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerMoveHandler
 {
     public ItemSO itemSO;
     public TMP_Text itemNameText;
     public TMP_Text priceText;
     public Image itemImage;
     [SerializeField] private ShopManager shopManager;
+    [SerializeField] private ShopInfo shopInfo;
     public int price;
 
     public void Initialize(ItemSO newItemSO, int price)
@@ -26,5 +28,26 @@ public class ShopSlot : MonoBehaviour
     public void OnBuyButtonClicked()
     {
         shopManager.TryBuyItem(itemSO, price);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (itemSO != null)
+        {
+            shopInfo.ShowItemInfo(itemSO);
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        shopInfo.HideItemInfo();
+    }
+
+    public void OnPointerMove(PointerEventData eventData)
+    {
+        if (itemSO != null)
+        {
+            shopInfo.FollowMouse();
+        }
     }
 }
