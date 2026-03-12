8191bf153c28de971250300bf0e6e54a96a8c07a
TechAoba <TechAoba@163.com>
2026-03-12 11:04:31 +0800

    店主动画

--- Assets/Scripts/Inventory&Shop/Shop/ShopKeeper.cs
+++ Assets/Scripts/Inventory&Shop/Shop/ShopKeeper.cs
@@ -12,6 +12,10 @@ public class ShopKeeper : MonoBehaviour
     [SerializeField] private List<ShopItems> shopItems;
     [SerializeField] private List<ShopItems> shopWeapons;
     [SerializeField] private List<ShopItems> shopArmours;
+
+    [SerializeField] private Camera shopkeeperCam;
+    [SerializeField] private Vector3 cameraOffset = new Vector3(0, 0, -1);
+
     public static event Action<ShopManager, bool> OnShopStateChanged;
     private bool playerInRange;
     private bool isShopOpen;
@@ -31,6 +35,9 @@ public class ShopKeeper : MonoBehaviour
                     shopCanvasGroup.alpha = 1;
                     shopCanvasGroup.blocksRaycasts = true;
                     shopCanvasGroup.interactable = true;
+
+                    shopkeeperCam.transform.position = transform.position + cameraOffset;
+                    shopkeeperCam.gameObject.SetActive(true);
                     OpenItemShop();
                 }
             }
@@ -43,6 +50,7 @@ public class ShopKeeper : MonoBehaviour
                 shopCanvasGroup.alpha = 0;
                 shopCanvasGroup.blocksRaycasts = false;
                 shopCanvasGroup.interactable = false;
+                shopkeeperCam.gameObject.SetActive(false);
             }
         }
     }
