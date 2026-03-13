394ead6e69897abdfc381b129fe9cde45bab3418
TechAoba <TechAoba@163.com>
2026-03-13 11:37:22 +0800

    添加交谈状态

---
+++ Assets/Scripts/NPC/NPC_Talk.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC_Talk : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator anim;
+    public Animator interactAnim;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    private void OnEnable()
+    {
+        rb.velocity = Vector2.zero;
+        rb.isKinematic = true;
+        anim.Play("Idle");
+        interactAnim.Play("Open");
+    }
+
+    private void OnDisable()
+    {
+        interactAnim.Play("Close");
+        rb.isKinematic = false;
+    }
+}
